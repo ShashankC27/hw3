611a29b8904876dd46343d784ec2ee5d482bef63
ShashankC27 <125625115+ShashankC27@users.noreply.github.com>
2023-03-26 20:23:27 -0700

    Add files via upload

--- my_mem_tb.sv
+++ my_mem_tb.sv
@@ -11,12 +11,12 @@ module my_mem_tb;
     
     integer i,size=6,error_count=0,j=0;
 
-    struct memorystructure {
+    struct {
         bit [15:0] Address_to_rw;
         bit [7:0] Data_to_Write;
         bit [7:0] Expected_data_Read;
         bit [7:0] Actual_data_Read;  
-    };
+    }memorystructure;
 
     initial begin
         clk=0;
