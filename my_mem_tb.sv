e55fe53aa524b21d3cd638eb863678af1493870d
ShashankC27 <125625115+ShashankC27@users.noreply.github.com>
2023-03-26 20:43:03 -0700

    Add files via upload

--- my_mem_tb.sv
+++ my_mem_tb.sv
@@ -17,7 +17,7 @@ module my_mem_tb;
         bit [7:0] Actual_data_Read;  
     } memorystructure;
 
-    memorystructure memarray[];
+    memorystructure memarray;
     my_mem tb(.clk(clk),.write(write),.read(read),.data_in(data_in),.address(address),.data_out(data_out));
     
     initial begin
